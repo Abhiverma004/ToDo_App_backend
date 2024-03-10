210cb54bf17a9bab3f03d5fe839033f90b5329ed
Abhiverma004 <abhiverma007shek@gmail.com>
2024-03-10 19:24:55 +0530

    first commit

---
+++ Server.js
@@ -0,0 +1,23 @@
+const express= require("express");
+const mongoose= require("mongoose");
+require ("dotenv").config();
+
+const routes=require("./routes/ToDoRoutes");
+
+const cors=require("cors");
+const app=express();
+const PORT=process.env.PORT || 5000
+
+app.use(cors())
+app.use(express.json())
+
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB Connected..."))
+.catch((err)=>console.log(err));
+
+app.use("/api",routes);
+
+app.listen(PORT,()=>{
+  console.log(`Listening at ${PORT}...`);
+})
